a1a8a8166da394a07678f9741eac791ebb7286a1
mcas2 <mcas2@tutanota.com>
2021-12-03 00:20:14 +0100

    First design attempts

--- app/src/main/java/com/mcas2/openrol/SplashScreen.java
+++ app/src/main/java/com/mcas2/openrol/SplashScreen.java
@@ -2,7 +2,15 @@ package com.mcas2.openrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +18,26 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView fondo = findViewById(R.id.backgroundSplash);
+        Glide.with(this)
+                .load(R.drawable.bc_orange_mountains)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .into(fondo);
+
+    }
+
+
+    public void openApp (boolean locationPermission){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
