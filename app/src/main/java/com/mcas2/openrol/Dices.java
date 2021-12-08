307d865791f38ea44377e32eccbb8200b3cce85f
mcas2 <mcas2@tutanota.com>
2021-12-08 21:52:19 +0100

    Dices more or less done

--- app/src/main/java/com/mcas2/openrol/Dices.java
+++ app/src/main/java/com/mcas2/openrol/Dices.java
@@ -8,17 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Dices extends AppCompatActivity {
     public EditText numDados;
     public EditText numCaras;
+    public EditText mod;
     public TextView result;
     public TextView resultHistory;
     public Button roll;
     public Button erase;
-    String history = "Dados: ";
+    public Button changeOp;
+    public boolean isNegative;
+    public String history = "Dados: ";
     Random r = new Random();
 
 
@@ -29,22 +31,30 @@ public class Dices extends AppCompatActivity {
 
         numDados = (EditText) findViewById(R.id.numDados);
         numCaras = (EditText) findViewById(R.id.numCaras);
+        mod = (EditText) findViewById(R.id.mod);
         result = (TextView) findViewById(R.id.result);
         resultHistory = (TextView) findViewById(R.id.resultHistory);
         roll = (Button) findViewById(R.id.rollDicesButton);
         erase = (Button) findViewById(R.id.eraseHistory);
+        changeOp = (Button) findViewById(R.id.changeOperator);
+        isNegative = false;
 
 
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int res = 0, diceRes;
+                int res = 0, diceRes, modifier;
                 int limit = Integer.parseInt(numCaras.getText().toString())+1;
+                modifier = Integer.parseInt(mod.getText().toString());
+
+                if (isNegative){
+                    modifier = modifier*-1;
+                }
 
                 for (int i = 0; i < Integer.parseInt(numDados.getText().toString()); i++) {
                     diceRes = r.nextInt(limit-1)+1;
-                    res += diceRes;
-                    history = history.concat(diceRes+",");
+                    res += (diceRes + modifier);
+                    history = history.concat((diceRes+modifier)+",");
                 }
                 result.setText(res+"");
                 resultHistory.setText(history);
@@ -56,6 +66,20 @@ public class Dices extends AppCompatActivity {
             public void onClick(View v) {
                 history = "Dados: ";
                 resultHistory.setText(history);
+            }
+        });
+
+        changeOp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isNegative){
+                    isNegative = false;
+                    changeOp.setText("+");
+                }
+                else {
+                    isNegative = true;
+                    changeOp.setText("-");
+                }
             }
         });
     }
