c8d52840d97b3f31ba39bdba49d7a4862d9f2b18
mcas2 <mcas2@tutanota.com>
2021-12-09 19:58:41 +0100

    Dices spinner view

--- app/src/main/java/com/mcas2/openrol/Dices.java
+++ app/src/main/java/com/mcas2/openrol/Dices.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class Dices extends AppCompatActivity {
     public EditText numDados;
-    public EditText numCaras;
     public EditText mod;
     public TextView result;
     public TextView resultHistory;
@@ -21,6 +25,9 @@ public class Dices extends AppCompatActivity {
     public Button changeOp;
     public boolean isNegative;
     public String history = "Dados: ";
+    public Integer [] diceFaces =  {4,6,8,10,12,20,100};
+    public Integer numCaras;
+    private ArrayAdapter adapter;
     Random r = new Random();
 
 
@@ -30,21 +37,38 @@ public class Dices extends AppCompatActivity {
         setContentView(R.layout.activity_dices);
 
         numDados = (EditText) findViewById(R.id.numDados);
-        numCaras = (EditText) findViewById(R.id.numCaras);
+
         mod = (EditText) findViewById(R.id.mod);
         result = (TextView) findViewById(R.id.result);
         resultHistory = (TextView) findViewById(R.id.resultHistory);
+
         roll = (Button) findViewById(R.id.rollDicesButton);
         erase = (Button) findViewById(R.id.eraseHistory);
         changeOp = (Button) findViewById(R.id.changeOperator);
         isNegative = false;
 
+        Spinner sp = (Spinner) findViewById(R.id.sp);
+        adapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, diceFaces);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                numCaras = (Integer) sp.getSelectedItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int res = 0, diceRes, modifier;
-                int limit = Integer.parseInt(numCaras.getText().toString())+1;
+                int limit = numCaras+1;
                 modifier = Integer.parseInt(mod.getText().toString());
 
                 if (isNegative){
@@ -54,7 +78,11 @@ public class Dices extends AppCompatActivity {
                 for (int i = 0; i < Integer.parseInt(numDados.getText().toString()); i++) {
                     diceRes = r.nextInt(limit-1)+1;
                     res += (diceRes + modifier);
-                    history = history.concat((diceRes+modifier)+",");
+                    if (modifier!=0){
+                        history = history.concat((diceRes+modifier)+"("+diceRes+"+"+modifier+")"+", ");
+                    } else {
+                        history = history.concat((diceRes+modifier)+", ");
+                    }
                 }
                 result.setText(res+"");
                 resultHistory.setText(history);
