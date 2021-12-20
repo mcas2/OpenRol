945e6ddf8bc1fe78f3ca04b683fd683b1b3cc91c
mcas2 <mcas2@tutanota.com>
2021-12-20 17:50:07 +0100

    developing first tab (basics) for dnd

--- app/src/main/java/com/mcas2/openrol/DnDCharFragment1.java
+++ app/src/main/java/com/mcas2/openrol/DnDCharFragment1.java
@@ -4,16 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DnDCharFragment1 extends Fragment {
+    private String [] classes = {"A","B"};
+    private String [] races = {"Enano","Elfo", "Mediano", "Humano", "Dracónido", "Gnomo"};
+    private ArrayAdapter classAdapter;
+    private ArrayAdapter raceAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dnd_1, container, false);
+        View view =  inflater.inflate(R.layout.fragment_dnd_1, container, false);
+
+        Spinner classSpinner = (Spinner) view.findViewById(R.id.classSpinnerDnD);
+        classAdapter = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item);
+        classAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        classSpinner.setAdapter(classAdapter);
+
+        Spinner raceSpinner = (Spinner) view.findViewById(R.id.raceSpinnerDnD);
+        raceAdapter = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item);
+        raceAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        raceSpinner.setAdapter(raceAdapter);
+        return view;
     }
 }
