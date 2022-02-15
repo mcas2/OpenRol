81c983e460315996069f23bb45ed28cbf5d2ca74
mcas2 <mcas2@tutanota.com>
2022-02-15 13:06:46 +0100

    Charsheet

--- app/src/main/java/com/mcas2/openrol/DnDCharacters.java
+++ app/src/main/java/com/mcas2/openrol/DnDCharacters.java
@@ -9,7 +9,11 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mcas2.openrol.ui.main.SectionsPagerAdapter;
 import com.mcas2.openrol.databinding.ActivityDndCharactersBinding;
@@ -17,6 +21,10 @@ import com.mcas2.openrol.databinding.ActivityDndCharactersBinding;
 public class DnDCharacters extends AppCompatActivity {
 
     private ActivityDndCharactersBinding binding;
+    private EditText strength;
+    private EditText dexterity;
+    private EditText constitution;
+    private TextView modStrength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,14 @@ public class DnDCharacters extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+
+        strength = (EditText) findViewById(R.id.dndCHeditTextStrength);
+        dexterity = (EditText) findViewById(R.id.dndCHeditTextDexterity);
+        constitution = (EditText) findViewById(R.id.dndCHeditTextConstitution);
+        modStrength = findViewById(R.id.dndCSModStrength);
+
+
+
         FloatingActionButton fab = binding.fab;
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +55,50 @@ public class DnDCharacters extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        strength.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                int numAbility = Integer.parseInt(String.valueOf(strength.getText()));
+                int modificador = assignAbilityScore(numAbility);
+                modStrength.setText(String.valueOf(modificador));
+                return false;
+            }
+        });
+
+    }
+
+    public int assignAbilityScore (int numAbility){
+        switch (numAbility){
+            case (3):
+                return -4;
+            case (4):
+            case (5):
+                return -3;
+            case (6):
+            case (7):
+                return -2;
+            case (8):
+            case (9):
+                return -1;
+            case (10):
+            case (11):
+                return 0;
+            case (12):
+            case (13):
+                return 1;
+            case (14):
+            case (15):
+                return 2;
+            case (16):
+            case (17):
+                return 3;
+            case (18):
+                return 4;
+            default:
+                Toast toast = Toast.makeText(getApplicationContext(), "Introduce un número entre 3 y 18", Toast.LENGTH_SHORT);
+                toast.show();
+                return 0;
+       }
     }
 }
--- app/src/main/java/com/mcas2/openrol/SelectCharSystem.java
+++ app/src/main/java/com/mcas2/openrol/SelectCharSystem.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class SelectCharSystem extends AppCompatActivity {
 
     private Button selCharDnD;
+    private Button selCharAquelarre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,15 @@ public class SelectCharSystem extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SelectCharSystem.this, SelectCharDnD.class);
+                startActivity(intent);
+            }
+        });
+
+        selCharAquelarre = findViewById(R.id.selCharAquelarre);
+        selCharAquelarre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectCharSystem.this, DnDCharacters.class);
                 startActivity(intent);
             }
         });
