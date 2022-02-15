883c92680f09e0ea2dec656a5f4480627671cdfa
mcas2 <mcas2@tutanota.com>
2022-02-15 18:12:14 +0100

    Avances en la hoja de personaje

--- app/src/main/java/com/mcas2/openrol/DnDCharFragments/DnDCharFragment1.java
+++ app/src/main/java/com/mcas2/openrol/DnDCharFragments/DnDCharFragment1.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,8 +17,18 @@ import androidx.fragment.app.Fragment;
 import com.mcas2.openrol.R;
 
 public class DnDCharFragment1 extends Fragment {
-    private ArrayAdapter classAdapter;
-    private ArrayAdapter raceAdapter;
+    private EditText strength;
+    private EditText dexterity;
+    private EditText constitution;
+    private EditText intelligence;
+    private EditText wisdom;
+    private EditText charisma;
+    private TextView modStrength;
+    private TextView modDexterity;
+    private TextView modConstitucion;
+    private TextView modIntelligence;
+    private TextView modWisdom;
+    private TextView modCharisma;
 
     @Nullable
     @Override
@@ -33,6 +46,114 @@ public class DnDCharFragment1 extends Fragment {
         raceAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
 
         raceSpinner.setAdapter(raceAdapter);
+
+        strength = (EditText) view.findViewById(R.id.dndCHeditTextStrength);
+        dexterity = (EditText) view.findViewById(R.id.dndCHeditTextDexterity);
+        constitution = (EditText) view.findViewById(R.id.dndCHeditTextConstitution);
+        intelligence = (EditText) view.findViewById(R.id.dndCHeditTextIntelligence);
+        wisdom = (EditText) view.findViewById(R.id.dndCHeditTextWisdom);
+        charisma = (EditText) view.findViewById(R.id.dndCHeditTextCharisma);
+        modStrength = view.findViewById(R.id.dndCSModStrength);
+        modDexterity = view.findViewById(R.id.dndCSModDexterity);
+        modConstitucion = view.findViewById(R.id.dndCSModConstitution);
+        modIntelligence = view.findViewById(R.id.dndCSModIntelligence);
+        modWisdom = view.findViewById(R.id.dndCSModWisdom);
+        modCharisma = view.findViewById(R.id.dndCSModCharisma);
+
+        strength.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String numAbilityText = String.valueOf(strength.getText());
+                Integer numAbility = Integer.parseInt(numAbilityText);
+                Integer modificador = assignAbilityScore(numAbility);
+                modStrength.setText(String.valueOf(modificador));
+            }
+        });
+
+        dexterity.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String numAbilityText = String.valueOf(dexterity.getText());
+                Integer numAbility = Integer.parseInt(numAbilityText);
+                Integer modificador = assignAbilityScore(numAbility);
+                modDexterity.setText(String.valueOf(modificador));
+            }
+        });
+
+        constitution.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String numAbilityText = String.valueOf(constitution.getText());
+                Integer numAbility = Integer.parseInt(numAbilityText);
+                Integer modificador = assignAbilityScore(numAbility);
+                modConstitucion.setText(String.valueOf(modificador));
+            }
+        });
+
+        intelligence.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String numAbilityText = String.valueOf(intelligence.getText());
+                Integer numAbility = Integer.parseInt(numAbilityText);
+                Integer modificador = assignAbilityScore(numAbility);
+                modIntelligence.setText(String.valueOf(modificador));
+            }
+        });
+
+        wisdom.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String numAbilityText = String.valueOf(wisdom.getText());
+                Integer numAbility = Integer.parseInt(numAbilityText);
+                Integer modificador = assignAbilityScore(numAbility);
+                modWisdom.setText(String.valueOf(modificador));
+            }
+        });
+
+        charisma.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String numAbilityText = String.valueOf(charisma.getText());
+                Integer numAbility = Integer.parseInt(numAbilityText);
+                Integer modificador = assignAbilityScore(numAbility);
+                modCharisma.setText(String.valueOf(modificador));
+            }
+        });
+
         return view;
+    }
+
+    public Integer assignAbilityScore (Integer numAbility){
+        switch (numAbility){
+            case (3):
+                return -4;
+            case (4):
+            case (5):
+                return -3;
+            case (6):
+            case (7):
+                return -2;
+            case (8):
+            case (9):
+                return -1;
+            case (10):
+            case (11):
+                return 0;
+            case (12):
+            case (13):
+                return 1;
+            case (14):
+            case (15):
+                return 2;
+            case (16):
+            case (17):
+                return 3;
+            case (18):
+                return 4;
+            default:
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Introduce un número entre 3 y 18", Toast.LENGTH_SHORT);
+                toast.show();
+                return 0;
+        }
     }
 }
--- app/src/main/java/com/mcas2/openrol/DnDCharacters.java
+++ app/src/main/java/com/mcas2/openrol/DnDCharacters.java
@@ -21,10 +21,7 @@ import com.mcas2.openrol.databinding.ActivityDndCharactersBinding;
 public class DnDCharacters extends AppCompatActivity {
 
     private ActivityDndCharactersBinding binding;
-    private EditText strength;
-    private EditText dexterity;
-    private EditText constitution;
-    private TextView modStrength;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +36,6 @@ public class DnDCharacters extends AppCompatActivity {
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
 
-        strength = (EditText) findViewById(R.id.dndCHeditTextStrength);
-        dexterity = (EditText) findViewById(R.id.dndCHeditTextDexterity);
-        constitution = (EditText) findViewById(R.id.dndCHeditTextConstitution);
-        modStrength = findViewById(R.id.dndCSModStrength);
-
-
 
         FloatingActionButton fab = binding.fab;
 
@@ -56,49 +47,7 @@ public class DnDCharacters extends AppCompatActivity {
             }
         });
 
-        strength.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                int numAbility = Integer.parseInt(String.valueOf(strength.getText()));
-                int modificador = assignAbilityScore(numAbility);
-                modStrength.setText(String.valueOf(modificador));
-                return false;
-            }
-        });
-
     }
 
-    public int assignAbilityScore (int numAbility){
-        switch (numAbility){
-            case (3):
-                return -4;
-            case (4):
-            case (5):
-                return -3;
-            case (6):
-            case (7):
-                return -2;
-            case (8):
-            case (9):
-                return -1;
-            case (10):
-            case (11):
-                return 0;
-            case (12):
-            case (13):
-                return 1;
-            case (14):
-            case (15):
-                return 2;
-            case (16):
-            case (17):
-                return 3;
-            case (18):
-                return 4;
-            default:
-                Toast toast = Toast.makeText(getApplicationContext(), "Introduce un número entre 3 y 18", Toast.LENGTH_SHORT);
-                toast.show();
-                return 0;
-       }
-    }
+
 }
